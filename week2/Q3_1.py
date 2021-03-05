1da1f19336e0cd3fda939947a5ba6cab56b6fa35
Natalia0604 <58288585+Natalia0604@users.noreply.github.com>
2021-03-05 16:16:41 +0800

    Update Q3_1.py

--- week2/Q3_1.py
+++ week2/Q3_1.py
@@ -1,19 +1,3 @@
-# def maxProduct(nums):
-#     n=[]
-#     for i in nums:
-#         if nums[0] < i:
-#             n.append(i)
-#             if nums[1] < i:
-
-      
-#     #print(nums[0])
-#     print(n)
-
-# # 請用你的程式補完這個函式的區塊
-# maxProduct([5, 20, 2, 6]) # 得到 120 因為 20 和 6 相乘得到最大值
-# maxProduct([10, -20, 0, 3]) # 得到 30 因為 10 和 3 相乘得到最大值
-# maxProduct([10, -20, 0, -30])
-
 def maxProduct(nums):
     #將nums由大到小排序
     for i in range(len(nums) - 1):
